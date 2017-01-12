ffb82a38c73ddcbdfacaa73985ef82fd88f45d1b
Eugen <evgeniy.sayenko@gmail.com>
2017-01-12 20:32:39 +0200

    Ореховская работа - рус. версия в основном сделана

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -1,6 +1,8 @@
 /**
  * Created by ASUS on 05.01.2017.
  */
+"use strict";
+
 var navButton = document.querySelector(".header__nav");
 var nav = document.querySelector(".mobile__nav");
 var close = document.querySelector(".mobile__nav-close");
@@ -15,4 +17,13 @@ close.addEventListener("click", function (event) {
     nav.classList.add("visually-hidden");
 });
 
+navButton.addEventListener("tap", function (event) {
+    event.preventDefault();
+    nav.classList.remove("visually-hidden");
+});
+
+close.addEventListener("tap", function (event) {
+    event.preventDefault();
+    nav.classList.add("visually-hidden");
+});
 
